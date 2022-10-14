75b6bbec9ba27147727b668d0aee6ad947bcf7b0
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-10-14 14:36:43 +0300

    Init react-router with root route

---
+++ src/routes/root.tsx
@@ -0,0 +1,13 @@
+export default function Root() {
+  return (
+    <>
+      <header>
+        <h1>Welcome to DPC React Router Post App</h1>
+        <nav>
+          <a href="/">Home</a>
+          <a href="posts">Posts</a>
+        </nav>
+      </header>
+    </>
+  );
+}
